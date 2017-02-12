10d786d703c511db76e98a5f229d9e9f3828a6a5
Talha Imam <smtalhaimam@gmail.com>
2017-02-12 17:11:26 +0500

    Factorial and Power Classes

---
+++ Factorial.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task2
+{
+    class Factorial
+    {
+        int num;
+        int num2 = 1;
+        int num3 = 1;
+        public int RecursiveFactorial(int n) //Recursive
+        {
+            this.num = n;
+            if (num > 0)
+            {
+                num = num * RecursiveFactorial(num - 1);
+                return num;
+            }
+            else
+                return num=1;
+        }
+        public void dislpayFactorial()
+        {
+            Console.WriteLine("Factorial by recursive: "+num);
+            Console.WriteLine("Factorial by for-loop:"+num2);
+            Console.WriteLine("Factorial by while-loop:" + num3);
+        }
+        public int LoopFactorial(int n) //For-Loop
+        {
+            if (n > 0)
+            {
+                for (int i = 1; i <= n; ++i)
+                {
+                    this.num2 = num2 * i;
+                }
+                return num2;
+            }
+            else
+            {
+                return num2=1;
+            }
+        }
+        public int WhileFactorial(int n) //While-loop
+        {
+            while (n > 0)
+            {
+                this.num3 = num3 * n;
+                n--;
+            }
+            return 1;
+        }
+    }
+}
---
+++ Power.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task2
+{
+    class Power
+    {
+        long p = 1;
+        public void doPower(int b)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                this.p = p * b;
+                Console.WriteLine("{0} power of {1}",b,i+" is: "+p);
+            }
+        }
+        public int RPower(int b, int power)
+        {
+            if (power != 0)
+            {
+                return b * RPower(b, power-1);
+            }
+            else
+                return 1;
+        }
+    }
+}
